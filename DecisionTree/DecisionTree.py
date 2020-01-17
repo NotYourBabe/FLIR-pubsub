6a29820ab3592cdcdb185ba75f9a7613a0ccfbdb
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-01-17 21:55:38 +0000

    rename plugin

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -17,4 +17,5 @@ def calcShannonEnt(dataSet):
 
 def createDataSet():
     dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
-    labels = ['no surfacing', 'flip
+    labels = ['no surfacing', 'flippers']
+    return dataSet, lab
