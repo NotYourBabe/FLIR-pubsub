887a8c96bdd223826b0fb517ddc9a47588217f67
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-01-17 22:11:03 +0000

    arm: asm: relaxed.h: defined relaxed.h

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -18,4 +18,6 @@ def calcShannonEnt(dataSet):
 def createDataSet():
     dataSet = [[1, 1, 'yes'], [1, 1, 'yes'], [1, 0, 'no'], [0, 1, 'no'], [0, 1, 'no']]
     labels = ['no surfacing', 'flippers']
-    return dataSet, lab
+    return dataSet, labels
+
+def splitDataSet(data
