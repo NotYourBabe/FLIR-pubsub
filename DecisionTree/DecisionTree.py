826bb5aca6c3c544f356618be141b18cb98b4bae
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-03-05 05:43:58 +0000

    Added entries for Oriya language Translation updation.

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -53,4 +53,5 @@ def majorityCnt(classList):
     classCount={}
     for vote in classList:
         if vote not in classCount.keys():
-            classCount[vote] = 
+            classCount[vote] = 0
+        classCount[vote] += 1
