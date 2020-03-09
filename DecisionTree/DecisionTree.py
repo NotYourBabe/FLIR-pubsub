bf45cb70b7efeae5def90f211e8ec38247720e09
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-03-09 23:04:17 +0000

    timer: optimize apply_slack()

--- DecisionTree/DecisionTree.py
+++ DecisionTree/DecisionTree.py
@@ -60,4 +60,4 @@ def majorityCnt(classList):
 
 def createTree(dataSet, labels):
     classList = [example[-1] for example in dataSet]
-    if classList.
+    if classList.count(classList[0]) == len(classL
