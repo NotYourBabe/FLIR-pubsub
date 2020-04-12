a1b4bb76a9aafc92ad328d098bdee55ed495bcd1
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-04-12 10:12:03 +0000

    adding next page functionality to AG

--- DecisionTree/paintTree.py
+++ DecisionTree/paintTree.py
@@ -8,4 +8,4 @@ def createPlot():
     fig = plt.figure(1, facecolor="white")
     fig.clf()
     createPlot.ax1 = plt.subplot(111, frameon=False)
-    plotNode('Decision 
+    plotNode('Decision Node', (0.5, 0.1), (0.1, 0.5
