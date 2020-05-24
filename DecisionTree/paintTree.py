8b9fabdff040390afd48add29c07da2b09a8464b
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-05-24 20:42:49 +0000

    Some tidying

--- DecisionTree/paintTree.py
+++ DecisionTree/paintTree.py
@@ -55,4 +55,5 @@ def plotTree(myTree, parentPt, nodeTxt):
     depth = getTreeDepth(myTree)
     firstStr = myTree.keys()[0]
     cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW,\
-        
+              plotTree.yOff)
+    p
