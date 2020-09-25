29dc86e9e2792d06a3ab5b27ecd10fb677d01437
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-09-25 20:03:10 +0000

    added build script

--- Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
+++ Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
@@ -17,4 +17,4 @@ using System.Windows;
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to acce
+// to COM components.  If you need to access a type in this assembly f
