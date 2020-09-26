8d692d742bf5aa9952aaeafc3c9004b008898074
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-09-26 13:12:17 +0000

    5.6.2

--- Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
+++ Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
@@ -18,4 +18,4 @@ using System.Windows;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible 
+// COM, set the ComVisible attribute to true on that ty
