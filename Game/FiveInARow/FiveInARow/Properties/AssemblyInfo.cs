541223b5c683c374c9686589727502ea0c6a1180
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-11-12 12:43:03 +0000

    New translations default.json (Spanish)

--- Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
+++ Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
@@ -37,4 +37,6 @@ using System.Windows;
                                      // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
                                               //(used if a resource is not found in the page, 
-                                              // app, or any the
+                                              // app, or any theme specific resource dictionaries)
+)]
+
