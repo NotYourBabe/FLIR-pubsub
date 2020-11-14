544a0eaa25ae114e7aae350f6f0ee44bcb42267b
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-11-14 22:48:47 +0000

    Read me update

--- Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
+++ Game/FiveInARow/FiveInARow/Properties/AssemblyInfo.cs
@@ -44,4 +44,5 @@ using System.Windows;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor 
+//      Minor Version 
+//      Build Number
