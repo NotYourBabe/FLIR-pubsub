d6a839f9c730afdb12e0c7d79d80e873fc9355e8
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2020-12-14 20:22:03 +0000

    Icons of Recipe Page

--- Game/FiveInARow/FiveInARow/Properties/Resources.Designer.cs
+++ Game/FiveInARow/FiveInARow/Properties/Resources.Designer.cs
@@ -13,4 +13,5 @@ namespace FiveInARow.Properties
 
 
     /// <summary>
-    ///   A strongly-typed resource class, for loo
+    ///   A strongly-typed resource class, for looking up localized strings, etc.
+   
