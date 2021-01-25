75eda2a2deeb0f58f34c3bf3af92adba726d3380
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2021-01-25 07:05:06 +0000

    amended readme

--- Game/FiveInARow/FiveInARow/Properties/Resources.Designer.cs
+++ Game/FiveInARow/FiveInARow/Properties/Resources.Designer.cs
@@ -37,4 +37,4 @@ namespace FiveInARow.Properties
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
-        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel
+        [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
