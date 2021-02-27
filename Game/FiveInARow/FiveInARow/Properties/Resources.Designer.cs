57e90dea9e86f0470c0546f025112a507c626fba
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2021-02-27 10:54:28 +0000

    few minor changes

--- Game/FiveInARow/FiveInARow/Properties/Resources.Designer.cs
+++ Game/FiveInARow/FiveInARow/Properties/Resources.Designer.cs
@@ -56,4 +56,5 @@ namespace FiveInARow.Properties
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.
+        internal static global::System.Globalization.CultureInfo Culture
+        
