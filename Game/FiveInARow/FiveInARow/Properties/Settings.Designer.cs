b445d71a9b5364d69c5fe7f9fe87b787305ce469
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2021-09-10 23:07:40 +0000

    Update blog.js

--- Game/FiveInARow/FiveInARow/Properties/Settings.Designer.cs
+++ Game/FiveInARow/FiveInARow/Properties/Settings.Designer.cs
@@ -13,4 +13,4 @@ namespace FiveInARow.Properties
 
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeA
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.
