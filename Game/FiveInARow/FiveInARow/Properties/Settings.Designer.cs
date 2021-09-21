d163b2a09fcf0f4b2944af2499b3c0fc6c42c8f4
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2021-09-21 13:30:32 +0000

    Version 2.2.4

--- Game/FiveInARow/FiveInARow/Properties/Settings.Designer.cs
+++ Game/FiveInARow/FiveInARow/Properties/Settings.Designer.cs
@@ -14,4 +14,4 @@ namespace FiveInARow.Properties
 
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal se
+    internal sealed partial class 
