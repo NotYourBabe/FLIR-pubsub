c8b07ef30e949aeebc3f2beb91baef8446a76b38
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2021-10-04 13:56:59 +0000

    adas

--- Game/FiveInARow/FiveInARow/Properties/Settings.Designer.cs
+++ Game/FiveInARow/FiveInARow/Properties/Settings.Designer.cs
@@ -17,4 +17,4 @@ namespace FiveInARow.Properties
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
     {
 
-        private static Settings defaultInstance = ((Settings)(global::System.Configur
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSetti
