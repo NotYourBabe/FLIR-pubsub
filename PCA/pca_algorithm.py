cc1ab64f0e6bd3a1913bc718827c2f02dc488423
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-04-23 06:59:57 +0000

    Update lists.js

--- PCA/pca_algorithm.py
+++ PCA/pca_algorithm.py
@@ -33,4 +33,5 @@ lowDMat, reconMat = pca(dataMat, 1)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
-ax.scatter(dataMat[:,0].flatten().A[0], dataMat[:,1].flatten().A[0], marker='^'
+ax.scatter(dataMat[:,0].flatten().A[0], dataMat[:,1].flatten().A[0], marker='^', s=90)
+ax.scatter(reconMat[:,
