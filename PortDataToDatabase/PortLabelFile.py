f2fbfa259db5440385fa2f84b9c73c7a77ce2e5c
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-04-30 09:22:28 +0000

    HID: uhid: forward open/close events to user-space

--- PortDataToDatabase/PortLabelFile.py
+++ PortDataToDatabase/PortLabelFile.py
@@ -2,4 +2,5 @@ import MySQLdb
 conn = MySQLdb.connect(host="localhost",
                        user="root",
                        passwd="1234",
-               
+                       db="testdb")
+x = conn.cur
