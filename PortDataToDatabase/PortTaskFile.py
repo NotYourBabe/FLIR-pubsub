f56f19f27a6f30f4a22b762a5a15d5728cffa589
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-05-22 22:38:04 +0000

    rt2x00: Identify ASUS USB-N53 device.

--- PortDataToDatabase/PortTaskFile.py
+++ PortDataToDatabase/PortTaskFile.py
@@ -12,3 +12,18 @@ for i in range(1, 11):
         skip = True
         for line in file:
             if (skip):
+                skip = False
+                continue
+            data = line.split(',')
+            try:
+                x.execute("""INSERT INTO testrecord VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""",
+                      (data[0], data[1], data[2],
+                       data[3], data[4], data[5],
+                       data[6], data[7], data[8],
+                       data[9],data[10], data[11]))
+                conn.commit()
+            except Exception as e:
+                conn.rollback()
+                #print(e)
+
+conn.close()
