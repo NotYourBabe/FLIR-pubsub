faffc752b02f615ee5d0b563c310b60165f9ec03
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-07-31 07:23:11 +0000

    gcc/

--- PortDataToDatabase/PortTaskFileToSingleAnswer.py
+++ PortDataToDatabase/PortTaskFileToSingleAnswer.py
@@ -20,4 +20,5 @@ for i in range(1, 11):
                 %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
                 %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
                 %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
-                %s,%s,%s,%s,%s,%s,%s,%s,
+                %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
+             
