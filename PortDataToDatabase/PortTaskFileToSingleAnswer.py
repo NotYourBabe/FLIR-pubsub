ad17f9586716852d3a1e7e36e76ac8abc17f9c71
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-08-04 07:06:41 +0000

    new notes

--- PortDataToDatabase/PortTaskFileToSingleAnswer.py
+++ PortDataToDatabase/PortTaskFileToSingleAnswer.py
@@ -23,4 +23,4 @@ for i in range(1, 11):
                 %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
                 %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,
                 %s,%s,%s,%s,%s)""",
-  
+                      
