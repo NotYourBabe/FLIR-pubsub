a31ed71d0cd64fc97d7879e729dbacd559939c7e
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-08-20 10:43:45 +0000

    Set version number.

--- PortDataToDatabase/PortTaskFileToSingleAnswer.py
+++ PortDataToDatabase/PortTaskFileToSingleAnswer.py
@@ -26,4 +26,5 @@ for i in range(1, 11):
                       (data[12], data[13], data[14], data[15], data[16],
                        data[17], data[18], data[19], data[20], data[21],
                        data[22], data[23], data[24], data[25], data[26],
-                       data[27], data[28], data[29], data[30],
+                       data[27], data[28], data[29], data[30], data[31],
+               
