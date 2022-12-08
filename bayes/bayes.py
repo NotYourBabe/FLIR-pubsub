61e2dc331176a14dd1c993c8ff0ab523859a9bf9
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-12-08 12:38:07 +0000

    removed examples

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -3,4 +3,5 @@ from numpy import *
 def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
-                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', '
+                   ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
+                  
