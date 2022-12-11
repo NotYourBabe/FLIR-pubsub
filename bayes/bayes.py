0d497fe50972135805b93b4cabb62720aae58678
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-12-11 07:44:38 +0000

    block: remove WQ_POWER_EFFICIENT from kblockd

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -4,4 +4,4 @@ def loadDataSet():
     postingList = [['my', 'dog', 'has', 'flea', 'problems', 'help', 'please'],
                    ['maybe', 'not', 'take', 'him', 'to', 'dog', 'park', 'stupid'],
                    ['my', 'dalmation', 'is', 'so', 'cute', 'I', 'love', 'him'],
-                   ['stop', 'posting', 'stupid', 
+                   ['stop', 'posting', 'stupid', 'worthless', 'garbage'],
