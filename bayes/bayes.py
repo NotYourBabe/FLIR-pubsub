903a5b500b3e682f73361c2cece498460c449d59
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2022-12-30 07:30:36 +0000

    travis build fix

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -42,3 +42,4 @@ def trainNB0(trainMatrix, trainCategory):
             p0Denom += sum(trainMatrix[i])
     p1Vect = log(p1Num / p1Denom)
     p0Vect = log(p0Num / p0Denom)
+    return p0Vect, p1Vect, pAbusive
