6e9f69d9ea2de556bed43e6c209a44277f7e80eb
NotYourBabe <95464000+NotYourBabe@users.noreply.github.com>
2023-01-08 12:45:07 +0000

    meh.

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -67,4 +67,4 @@ def testingNB():
         trainMatrix.append(setOfWords2Vec(myVocabList, doc))
     p0V, p1V, pAb = trainNB0(trainMatrix, listClasses)
     testEntry = ['love', 'my', 'dalmation']
-    thi
+    thisDoc = array(setOfWords2Vec(myVocab
